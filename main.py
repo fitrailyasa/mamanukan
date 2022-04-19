65b2e9d19f6a8c9473c70c321ad19a9bf0d62691
Fitra Ilyasa <85192292+fitrailyasa@users.noreply.github.com>
2022-04-19 22:27:17 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,46 @@
+# Kelas Game
+Class Game:
+  # Konstruktor
+  def __init__(self):
+    pass
+  
+  # Method tampilan awal
+  def tampilan_awal(self):
+    pass
+    
+  # Method memulai
+  def mulai(self):
+     pass
+     
+  # Method kecepatan
+  def kecepatan(self):
+    pass
+  
+  # Method pipa awal
+  def pipa_awal(self):
+    pass
+  
+  # Method pipa yg lain
+  def update_pipa(self):
+    pass
+  
+  # Method refresh
+  def refresh(self):
+    pass
+  
+  # Method menampilkan skor
+  def tampil_skor(self):
+    pass
+  
+  # Method selesai (game over)
+  def selesai(self):
+    pass
+  
+Class Manuk:
+
+
+Class Jalan:
+
+
+Class Pipa:
+
